e4260ed4201be67e3cac4dd6f0175ae2e59dc2be
darren-rose <darrenwrose@gmail.com>
2021-03-30 00:07:03 +0200

    feat(foo,boo): add foo and boo commands

--- cmd/root.go
+++ cmd/root.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
@@ -29,16 +30,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "cli-helloworld",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "hello world",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello World")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
